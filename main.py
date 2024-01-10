4e6dc9041730ffa641d16777e62aa7830e223266
Bassem Ahmed <112704843+Bassem-2000@users.noreply.github.com>
2024-01-10 22:45:05 +0200

    Update main.py

--- main.py
+++ main.py
@@ -4,11 +4,16 @@ import pickle
 from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 from tensorflow.keras.preprocessing.text import Tokenizer
+import concurrent.futures
 
-loaded_model = load_model('model.h5')
+# Load the model
+loaded_model = load_model('my_model.h5')
 
+# Load the tokenizer
 with open('tokenizer.pickle', 'rb') as handle:
     loaded_tokenizer = pickle.load(handle)
+
+# Maximum sequence length used during training
 
 maxlen = 20
 
@@ -17,15 +22,27 @@ app = FastAPI()
 class TextRequest(BaseModel):
     text: str
 
+def predict(text):
+    # Preprocess the text
+    sequence = loaded_tokenizer.texts_to_sequences([text])
+    padded_sequence = pad_sequences(sequence, maxlen=maxlen)
+    
+    # Make prediction using the loaded model
+    prediction = loaded_model.predict(padded_sequence)
+    # Assuming the output is a probability, you might want to return a rounded value (0 or 1)
+    rounded_prediction = int(round(prediction[0][0]))
+    
+    return rounded_prediction
+
 @app.post("/predict/")
 async def predict_sentiment(request: TextRequest):
     text = request.text
     
-    sequence = loaded_tokenizer.texts_to_sequences([text])
-    padded_sequence = pad_sequences(sequence, maxlen=maxlen)
-    
-    prediction = loaded_model.predict(padded_sequence)
-    rounded_prediction = int(round(prediction[0][0]))
+    # Use ThreadPoolExecutor to parallelize predictions
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        result = executor.submit(predict, text)
+        prediction = result.result()
+        
     if rounded_prediction == 1:
         return {"prediction: القاهرة"}
     if rounded_prediction == 0:
